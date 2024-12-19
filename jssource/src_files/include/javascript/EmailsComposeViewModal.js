0464c4a15d18727f8d4c609361321a5755362e87
enricsinergia <57436616+enricsinergia@users.noreply.github.com>
2024-12-19 09:18:49 +0100

    Hotfix - Personas y Organizaciones - Condición para aviso cuando email masivo (#504)

--- jssource/src_files/include/javascript/EmailsComposeViewModal.js
+++ jssource/src_files/include/javascript/EmailsComposeViewModal.js
@@ -118,12 +118,14 @@
 
   $.fn.openComposeViewModal = function (source) {
     "use strict";
-    
+
     window.event.preventDefault();
     window.event.stopImmediatePropagation();
     // STIC-Custom 20241003 EPS - Warning when entire list selected
     // https://github.com/SinergiaTIC/SinergiaCRM/pull/418
-    if($("[name='select_entire_list']").val() == 1) {
+    // https://github.com/SinergiaTIC/SinergiaCRM/pull/504
+    // if($("[name='select_entire_list']").val() == 1) {
+    if(document.MassUpdate.select_entire_list.value == 1) {
       alert(SUGAR.language.get('app_strings', 'LBL_EMAIL_SENDING_ALERT_CURRENT_PAGE'));
     }
     // END STIC-Custom
