635b13c517069ff3e34eaba9c957faa4f37db6b0
enricsinergia <57436616+enricsinergia@users.noreply.github.com>
2024-12-20 14:24:52 +0100

    Hotfix - KReports - Deshacer los cambios del PR #505 (#524)

--- modules/KReports/KReportQuery.php
+++ modules/KReports/KReportQuery.php
@@ -1548,9 +1548,12 @@ class KReportQuery {
 
    function getWhereOperatorClause($operator, $fieldname, $fieldid, $path, $value, $valuekey, $valueto, $valuetokey = '', $jointype = '') {
       global $current_user;
-      // STIC-Custom EPS 20241205
-      // https://github.com/SinergiaTIC/SinergiaCRM/pull/505
-      $db = DBManagerFactory::getInstance();
+      // STIC-Custom EPS 20211220 - Anular el PR #505
+      // https://github.com/SinergiaTIC/SinergiaCRM/pull/524
+      // // STIC-Custom EPS 20241205
+      // // https://github.com/SinergiaTIC/SinergiaCRM/pull/505
+      // $db = DBManagerFactory::getInstance();
+      // // ENS STIC-Custom
       // ENS STIC-Custom
 
       // initialize
@@ -1648,9 +1651,12 @@ class KReportQuery {
             break;
       }
       // process the operator
-      // STIC-Custom EPS 20241205 - quote the value to avoid errors when certain values are used (apostrophe)
-      // https://github.com/SinergiaTIC/SinergiaCRM/pull/505
-      $value = $db->quote($value);
+      // STIC-Custom EPS 20211220 - Anular el PR #505
+      // https://github.com/SinergiaTIC/SinergiaCRM/pull/524
+      // // STIC-Custom EPS 20241205 - quote the value to avoid errors when certain values are used (apostrophe)
+      // // https://github.com/SinergiaTIC/SinergiaCRM/pull/505
+      // $value = $db->quote($value);
+      // // END STIC-Custom
       // END STIC-Custom
       switch ($operator) {
          case 'autocomplete':
