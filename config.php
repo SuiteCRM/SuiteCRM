00f4573f8b8778f15f5a0cec777fe538a917e444
Paula Carmona Sánchez <125350097+PaulaaSTIC@users.noreply.github.com>
2025-07-30 14:10:27 +0200

    SinergiaCRM 2.2.1 Release (#753)

--- config.php
+++ config.php
@@ -611,8 +611,8 @@ $sugar_config = array(
     
     // STIC Custom 20231124 JBL - SemVer in SinergiaCRM
     // STIC#1319
-    'sinergiacrm_version' => '2.2.0', 
-    'js_custom_version' => 17,
+    'sinergiacrm_version' => '2.2.1', 
+    'js_custom_version' => 18,
     'stic_show_update_alert' => 1,
     // END STIC
 
