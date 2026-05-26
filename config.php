7e91a0bd52d53a195293bc36b2db3869e66725f9
Ainara Ramos <ainara@sinergiacrm.org>
2026-05-26 11:24:40 +0200

    SinergiaCRM Release 2.10.1 (#1190)

--- config.php
+++ config.php
@@ -611,9 +611,9 @@ $sugar_config = array(
     
     // STIC Custom 20231124 JBL - SemVer in SinergiaCRM
     // STIC#1319
-    'sinergiacrm_version' => '2.10.0', 
-    'js_custom_version' => 37,
-    'stic_show_update_alert' => 1,
+    'sinergiacrm_version' => '2.10.1', 
+    'js_custom_version' => 38,
+    'stic_show_update_alert' => 0,
     // END STIC
 
     // STIC-Custom 20240117 JCH - Security Groups Module Rules activation config
